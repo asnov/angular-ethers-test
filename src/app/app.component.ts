049e5d8cf9407fce9bf96d1f56079ab6f52cb4a0
Alex Novikov on mac-mini <alex@asnovikov.ru>
2018-09-17 11:28:09 -0300

    added ethers & ethers2;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+// this gives Uncaught Error: Cannot find module 'tslib'
+import { ethers } from 'ethers';
+
+// this works nicely:
+// import { ethers } from 'ethers2';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ethers-test';
+
+  constructor() {
+    const bn: ethers.BigNumber = ethers.utils.bigNumberify(1);
+    console.log(`BigNumber=`, bn);
+  }
+
 }
