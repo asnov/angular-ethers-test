bf8588072610b8cbaf1a54dc59e931de58a69cbb
Alex Novikov <alex@asnovikov.ru>
2018-10-30 20:14:10 -0300

    switched to liquid-long@1.2.2 & ethers@4.0.4;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,14 @@ import { AsyncSendable, Web3Provider } from 'ethers/providers';
 // import { ethers } from 'ethers2';
 
 // This compiles but intellisence doesn't work
-// import { LiquidLong, Provider, Scheduler, TimeoutScheduler, JsonRpcProvider } from '@keydonix/liquid-long-client-library';
+import { LiquidLong, Provider, Scheduler, TimeoutScheduler, JsonRpcProvider, Signer } from '@keydonix/liquid-long-client-library';
 
 // "source/index.ts is missing from the TypeScript compilation" error
 // import { LiquidLong, Provider, Scheduler, TimeoutScheduler, JsonRpcProvider } from '@keydonix/liquid-long-client-library/source';
 
 // This works fine but looks weird
-import { LiquidLong, Provider, Scheduler, TimeoutScheduler, JsonRpcProvider } from '@keydonix/liquid-long-client-library/output';
-import 'ethers/shims/xmlhttprequest';
+// import { LiquidLong, Provider, Scheduler, TimeoutScheduler, JsonRpcProvider } from '@keydonix/liquid-long-client-library/output';
+// import 'ethers/shims/xmlhttprequest';
 
 
 declare const web3: {
@@ -42,13 +42,14 @@ export class AppComponent {
     const scheduler: Scheduler = new TimeoutScheduler();
     // const provider: Provider = new JsonRpcProvider('http://localhost:8545');
     const provider: Provider = new Web3Provider(web3.currentProvider);
+    const signer: Signer = null;
 
     // const provider: JsonRpcProvider = new JsonRpcProvider('http://localhost:8545');
     // const provider: Web3Provider = new Web3Provider(web3.currentProvider);
     const liquidLongAddress = '0x80F8DAA435A9AB4B1802BA56FE7E0ABD0F8AB3D3';
     const defaultEthPriceInUsd = 225;
     const defaultProviderFeeRate = 0.21;
-    const ll: LiquidLong = new LiquidLong(scheduler, provider, liquidLongAddress, defaultEthPriceInUsd, defaultProviderFeeRate);
+    const ll: LiquidLong = new LiquidLong(scheduler, provider, signer, liquidLongAddress, defaultEthPriceInUsd, defaultProviderFeeRate);
     await ll.awaitReady;
   }
 
